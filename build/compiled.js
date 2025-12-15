d406f2be15ab7a3d0d43382382be110383e75678
Marc Doerr <m_doerr14@cs.uni-kl.de>
2025-12-15 13:05:40 +0100

    chore: lint

--- build/compiled.js
+++ build/compiled.js
@@ -72,6 +72,8 @@
 
   // src/utils/lodash-impl.ts
   function escape(input) {
+    if (typeof input !== "string")
+      return "";
     const s = String(input ?? "");
     return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
   }
