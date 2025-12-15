b0a5167352f34936429faaf6899e47b4205c6003
Marc Doerr <m_doerr14@cs.uni-kl.de>
2025-12-15 13:08:52 +0100

    chore: format

--- src/utils/lodash-impl.ts
+++ src/utils/lodash-impl.ts
@@ -1,9 +1,9 @@
 // Dirty re-implementation of lodash-functions because I do not want to rely on too many external dependencies and blow up output.
 
 export function escape(input: unknown): string {
-  if (typeof input !== "string") return "";
+  if (typeof input !== 'string') return '';
 
-  const s = String(input ?? "");
+  const s = String(input ?? '');
   return s
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
